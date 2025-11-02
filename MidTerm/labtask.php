6d73c1da0d48da867e7d8183e228ae7708870624
Sabbir-Hossain-03 <sabbirhossain01963@gmail.com>
2025-11-02 12:28:02 +0600

    Update and rename lab1.php to labtask.php

---
+++ MidTerm/labtask.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<head>
+
+</head>
+<body>
+    <table>
+    <center>
+    <h1 style="text-size:70px">Clinic Patient Registration</h1>
+</center>
+    Full Name:<br>
+    <input type="text"><br><br>
+    Age:<br>
+    <input type="text"><br><br>
+    Phone Number:<br>
+    <input type="text"><br><br>
+    Email Address:<br>
+    <input type="text"><br><br>
+    Insurance Provider:<br>
+    <select>
+        <option>Select Provider</option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <input type="checkbox"> 1
+        <input type="checkbox"> 1
+</select><br><br>
+Insurance Policy Number:<br>
+    <input type="text"><br><br>
+    <center>
+    <h1>Additional Information:</h1>
+</center>
+    Username:<br>
+    <input type="text"><br><br>
+    Password:<br>
+    <input type="text"><br><br>
+    Cofirm Password:<br>
+    <input type="text"><br><br>
+    <center>
+    <input type="submit" value ="Register">
+</center>
+</table>
+</body>
